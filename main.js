d39443360b742197fa349d52d165e3113cad31ad
Paul Stonjek <paul.stonjek@meteoblue.com>
2019-10-02 17:25:45 +0200

    added a bunch of stuff

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 // create PaintRoom object and implement functions
 var pr = {}
+
+// do things only when document has finished loading
+$("document").ready(function(){
+    // use paint.js
+    var drawspace = document.getElementById("drawspace");
+    pr.paint = new Paint(drawspace);
+
+    pr.paint.addEventListener("drawing", function (event) {
+        console.log(event.type);
+        console.log(event.drawing);
+    });
+})
