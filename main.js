349234eae47c627ecad1277636a5d80e74f1098a
Thundergrass <stonjekp@dhbw-loerrach.de>
2019-10-20 17:12:09 +0200

    add at least some resemblance of functionality

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
 // create PaintRoom object and implement functions
 var pr = {}
 
+pr.logpaint = () => {
+    console.log(pr.paint);
+}
+pr.fireDrawing = () => {
+    pr.paint.addUserDrawing({
+        type: "line",
+        x: 230,
+        y: 230,
+        x1: 1350,
+        y1: 230,
+        size: 30,
+        color: tinycolor("red"),
+    });
+}
+pr.outputCanvas = () => {
+    console.log(pr.paint.localDrawings);
+    console.log(pr.paint.localUserPaths);
+}
+
 // do things only when document has finished loading
 $("document").ready(function(){
     // use paint.js
     var drawspace = document.getElementById("drawspace");
     pr.paint = new Paint(drawspace);
 
-    drawspace.addEventListener("userdrawing", function (event) {
+    pr.paint.addEventListener("userdrawing", function (event) {
         console.log(event);
-        // console.log(event.type);
-        // console.log(event.drawing);
+    });
+    pr.paint.addEventListener("startuserpath", (event) => {
+        console.log("user started drawing:");
+        console.log(event);
+    });
+    pr.paint.addEventListener("enduserpath", (event) => {
+        console.log("user stopped drawing:");
+        console.log(event);
     });
     console.log("ready");
     console.log(pr.paint);
+
+    $("#testbox").append("<button onClick='pr.logpaint()'>log paint</button>")
+    $("#testbox").append("<button onClick='pr.fireDrawing()'>fire draw event</button>")
+    $("#testbox").append("<button onClick='pr.fireDrawing()'>output canvas</button>")
+    $("#testbox").append("<button onClick='pr.paint.clear()'>clear canvas</button>")
 });
--- raw.js
+++ raw.js
@@ -3,7 +3,7 @@ $("document").ready(function () {
     var container = document.getElementById("drawspace");
     var paint = new Paint(container);
 
-    paint.addEventListener("drawing", function (event) {
+    paint.addEventListener("userdrawing", function (event) {
         console.log(event.type);
         console.log(event.drawing);
     });
